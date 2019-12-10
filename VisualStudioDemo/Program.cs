f489966ef55fe452b08f800ca130de5d46ccfc02
William Osbourn <william@osbourn.us>
2019-12-10 14:35:48 -0600

    Added a line

--- VisualStudioDemo/Program.cs
+++ VisualStudioDemo/Program.cs
@@ -6,7 +6,8 @@ namespace VisualStudioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am a changing it");
+            Console.ReadLine(); 
         }
     }
 }

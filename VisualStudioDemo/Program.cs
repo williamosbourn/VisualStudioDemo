cfed54f97a5874451cce1c9e920de5d6540325d8
williamosbourn <william@osbourn.us>
2019-12-19 09:24:45 -0600

    I am configuring my github

--- VisualStudioDemo/Program.cs
+++ VisualStudioDemo/Program.cs
@@ -6,7 +6,7 @@ namespace VisualStudioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I am a changing it");
+            Console.WriteLine("I am configuring my github.");
             Console.ReadLine(); 
         }
     }
